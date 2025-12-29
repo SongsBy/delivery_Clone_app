ca649e1ecd417623f77ebdf72aeeff85b20b7a85
songjeonghun <songzh0415@naver.xom>
2025-12-29 13:35:42 +0900

    12/29

---
+++ lib/common/const/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+//주색상
+const PRIMARY_COLOR = Color(0XFF22A45D);
+//텍스트 색상
+const BODY_COLOR = Color(0XFF868686);
+//텍스트 필드 배경 색상
+const INPUT_BG_COLOR = Color(0XFFFBFBFB);
+//텍스트 필드 테두리 색상
+const BORDER_COLOR = Color(0XFFF3F2F2);
---
+++ lib/common/const/component/custom_text_form_field.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:project01/common/const/colors.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String? hintText;
+  final String? errorText;
+  final bool obscureText;
+  final bool autoFocus;
+  final ValueChanged<String>? onChanged;
+
+  const CustomTextFormField({
+    this.hintText,
+    this.errorText,
+    this.obscureText = false,
+    this.autoFocus = false,
+    required this.onChanged,
+    super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final baseBorder = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: BORDER_COLOR,
+        width: 2.0
+      ),
+    );
+    return TextFormField(
+      cursorColor: PRIMARY_COLOR,
+      //비밀번호 입력할때
+      obscureText: obscureText,
+      //자동 커서 할당
+      autofocus: autoFocus,
+      onChanged: onChanged,
+
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(20),
+        errorText: errorText,
+        hintText: hintText,
+        hintStyle: TextStyle(
+            color: BODY_COLOR,
+          fontSize: 14.0,
+        ),
+        fillColor: INPUT_BG_COLOR,
+        filled: true,
+        //모든 인풋 상태의 스타일
+        border: baseBorder,
+        focusedBorder: baseBorder.copyWith(
+          borderSide: baseBorder.borderSide.copyWith(
+            color: PRIMARY_COLOR,
+          )
+        )
+      ),
+    );
+  }
+}
