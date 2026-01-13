d0928f7631726669e9af82cd55a9828756f4ad51
songjeonghun <songzh0415@naver.xom>
2026-01-13 21:56:29 +0900

    1/13

--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -9,8 +9,32 @@ class RootTab extends StatefulWidget {
   State<RootTab> createState() => _RootTabState();
 }
 
-class _RootTabState extends State<RootTab> {
+class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin{
+  late TabController controller;
   int index = 0;
+
+  @override
+  void initState() {
+    super.initState();
+   controller = TabController(length: 4, vsync: this); //vsync에는  SingleTickerProviderStateMixin 이 필수 이다
+    // 2. 리스너 부착 (감시자 붙이기)
+// 컨트롤러의 상태가 변할 때마다 tabListener 함수를 실행해라!
+    controller.addListener(tabListener);
+  }
+  @override
+  void dispose() {
+    controller.removeListener(tabListener);
+    super.dispose();
+  }
+
+
+  void tabListener(){
+    setState(() {
+      // 컨트롤러가 현재 가리키는 페이지 번호(index)를 가져와서
+      // 내 화면의 상태(index)로 동기화시킨다.
+      index = controller.index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
@@ -22,9 +46,7 @@ class _RootTabState extends State<RootTab> {
             unselectedFontSize: 10,
             type: BottomNavigationBarType.fixed,
             onTap: (int index){
-            setState(() {
-              this.index = index;
-            });
+            controller.animateTo(index);
             },
             currentIndex: index,
             items: [
@@ -47,8 +69,13 @@ class _RootTabState extends State<RootTab> {
             ]
         ),
         child: TabBarView(
+          physics: NeverScrollableScrollPhysics(),
+          controller: controller,
             children: [
-
+              Center(child: Container(child: Text('홈'))),
+              Center(child: Container(child: Text('음식'))),
+              Center(child: Container(child: Text('주문'))),
+              Center(child: Container(child: Text('프로필'))),
             ]
         )
     );
